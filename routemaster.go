bf454fff4a64e94e294e4bb001caa4f8d0a22b2a
Karl Entwistle <karl@entwistle.com>
2013-04-28 21:20:08 +0100

    First working version, loads of refactoring left to do

--- routemaster.go
+++ routemaster.go
@@ -1,61 +1,140 @@
 package main
 
 import (
-  "net/http"
-  "fmt"
-  "io/ioutil"
-  "log"
-  "encoding/json"
-  "flag"
-  "net"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"route53"
 )
 
 var CHECKIP_URL = "http://whatismyip.herokuapp.com/"
 
 type AwsAccessIdentifier struct {
-  AccessKey string `json:"access_key"`
-  SecretKey string `json:"secret_key"`
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
 }
 
-func parseAccessIdentifierJSON(data []byte) (AwsAccessIdentifier, error) {
-  var accessIdentifier AwsAccessIdentifier
-  err := json.Unmarshal(data, &accessIdentifier)
-  return accessIdentifier, err
+func parseAccessIdentifierJSON(data []byte) (route53.AccessIdentifiers, error) {
+	var accessIdentifier AwsAccessIdentifier
+	err := json.Unmarshal(data, &accessIdentifier)
+	return route53.AccessIdentifiers{
+		AccessKey: accessIdentifier.AccessKey,
+		SecretKey: accessIdentifier.SecretKey,
+	}, err
 }
 
-func readLocalFile(location string) string {
-  contents, err := ioutil.ReadFile(location)
-  if err != nil {
-    log.Fatal("ReadLocalFile Error:", contents, err)
-  }
-  return (string(contents))
+func readLocalFile(location string) (c []byte, err error) {
+	c, err = ioutil.ReadFile(location)
+	if err != nil {
+		log.Fatal("ReadLocalFile Error:", c, err)
+	}
+	return
 }
 
 func getWanIP(url string) (net.IP, error) {
-  resp, err := getBody(url)
-  if err == nil {
-    return net.ParseIP(string(resp)), nil  
-  }
-  return nil, err
+	resp, err := getBody(url)
+	if err == nil {
+		return net.ParseIP(string(resp)), nil
+	}
+	return nil, err
 }
 
 func getBody(url string) ([]byte, error) {
-  client := &http.Client{}
-  req, err := http.NewRequest("GET", url, nil)
-  if err == nil {
-    resp, err := client.Do(req)
-    defer resp.Body.Close()
-    if err == nil {
-      return ioutil.ReadAll(resp.Body)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err == nil {
+		resp, err := client.Do(req)
+		defer resp.Body.Close()
+		if err == nil {
+			return ioutil.ReadAll(resp.Body)
+		}
+	}
+	return nil, err
+}
+
+//TODO add test
+func findZone(zones route53.HostedZones) (hz route53.HostedZone) {
+	for i := range zones.HostedZones {
+		if zones.HostedZones[i].Name == *hosted_zone {
+			return zones.HostedZones[i]
+		}
+	}
+	return hz
+}
+
+//TODO add test
+func findRecord(records route53.ResourceRecordSets) (rrs route53.ResourceRecordSet) {
+  for i := range records.ResourceRecordSets {
+    if records.ResourceRecordSets[i].Name == *subdomain + "." + *hosted_zone {
+      return records.ResourceRecordSets[i]
     }
   }
-  return nil, err
+  return rrs
+}
+
+func updateRecord(zone route53.HostedZone, aws route53.AccessIdentifiers, action string, name string, value string) {
+  var create = route53.ChangeResourceRecordSetsRequest{
+    ZoneID: zone.HostedZoneId(),
+    Comment: "",
+    Changes: []route53.Change{
+      {
+        Action:        action,
+        Name:          name,
+        Type:          "A",
+        TTL:           300,
+        Value:         value,
+      },
+    },
+  }
+
+  r, err := create.Create(aws)
+
+  if err != nil {
+    log.Fatal("Update record failed:", r, err)
+  }
 }
 
 var aws_secrets *string = flag.String("secrets-file", "", "/path_to/.your_aws_secrets")
+var hosted_zone *string = flag.String("hosted-zone", "", "[your hosted zone]")
+var subdomain *string = flag.String("subdomain", "", "[your subdomain]")
 
 func main() {
-  flag.Parse()
-  fmt.Println(getWanIP(CHECKIP_URL))
-  
+	flag.Parse()
+  wanIP, err := getWanIP(CHECKIP_URL)
+
+  if err != nil {
+    log.Fatal("Failed to fetch current IP:", err)
+  }
+
+  fmt.Println("IP is " + wanIP.String())
+	contents, _ := readLocalFile(*aws_secrets)
+	aws, _ := parseAccessIdentifierJSON(contents)
+	zone := findZone(aws.Zones())
+	resourceRecordSets, err := zone.ResourceRecordSets(aws)
+
+  if err != nil {
+    log.Fatal("Resource Record Sets Invalid:", resourceRecordSets, err)
+  }
+
+	record := findRecord(resourceRecordSets)
+
+  if record.Name == "" {
+    updateRecord(zone, aws, "CREATE", *subdomain + "." + *hosted_zone, wanIP.String())
+    log.Fatal("A record with name " + *subdomain +" was not found, created")    
+  } 
+
+  fmt.Println("IP was " + record.Value[0] )
+
+  if record.Value[0] == wanIP.String() {
+    log.Fatal("Nothing to do")
+  }
+
+  fmt.Println("Updating IP with Route53")
+  updateRecord(zone, aws, "DELETE", *subdomain + "." + *hosted_zone, record.Value[0])
+  updateRecord(zone, aws, "CREATE", *subdomain + "." + *hosted_zone, wanIP.String())
+  fmt.Println("Done")
 }
--- routemaster_test.go
+++ routemaster_test.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  "io"
-  "testing"
-  "strings"
-  "net/http"
-  "net/http/httptest"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
 var ACCESS_IDENTIFIERS = `{
@@ -15,63 +15,70 @@ var ACCESS_IDENTIFIERS = `{
 
 var LOCAL_FILE = `routemaster_test.go`
 
-
 type emptyHandler struct{}
 
 func (h *emptyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Test-Header", "Testing")
-  io.WriteString(w, "127.0.0.1")
+	w.Header().Set("Test-Header", "Testing")
+	io.WriteString(w, "127.0.0.1")
 }
 
-
 func TestRawRemoteUrl(t *testing.T) {
-  handler := &emptyHandler{}
-  server := httptest.NewServer(handler)
-  resp, err := getBody(server.URL)
+	handler := &emptyHandler{}
+	server := httptest.NewServer(handler)
+	resp, err := getBody(server.URL)
 
-  if err != nil {
-    t.Fatal("Error:", err)
-  }
+	if err != nil {
+		t.Fatal("Error:", err)
+	}
 
-  if strings.Contains(string(resp), "127.0.0.1") != true {
-    t.Fatal("Webpage did not return", resp)
-  }
+	if strings.Contains(string(resp), "127.0.0.1") != true {
+		t.Fatal("Webpage did not return", resp)
+	}
 }
 
 func TestGetWanIP(t *testing.T) {
-  handler := &emptyHandler{}
-  server := httptest.NewServer(handler)
-  ip, err := getWanIP(server.URL)
+	handler := &emptyHandler{}
+	server := httptest.NewServer(handler)
+	ip, err := getWanIP(server.URL)
 
-  if err != nil {
-    t.Fatal("Error:", err)
-  } 
+	if err != nil {
+		t.Fatal("Error:", err)
+	}
 
-  if ip.String() != "127.0.0.1" {
-    t.Fatal("Invalid IP address", ip)
-  }
+	if ip.String() != "127.0.0.1" {
+		t.Fatal("Invalid IP address", ip)
+	}
 }
 
 func TestParseAccessIdentifiers(t *testing.T) {
-  accessIdentifier, error := parseAccessIdentifierJSON([]byte(ACCESS_IDENTIFIERS))
+	accessIdentifier, error := parseAccessIdentifierJSON([]byte(ACCESS_IDENTIFIERS))
 
-  if error != nil {
-    t.Fatalf("error parsing JSON", accessIdentifier, error)
-  }
+	if error != nil {
+		t.Fatalf("error parsing JSON", accessIdentifier, error)
+	}
 
-  if accessIdentifier.AccessKey != "022QF06E7MXBSH9DHM02" {
-    t.Fatalf("incorrect JSON parsing element mismatch", accessIdentifier)  
-  }
+	if accessIdentifier.AccessKey != "022QF06E7MXBSH9DHM02" {
+		t.Fatalf("incorrect JSON parsing element mismatch", accessIdentifier)
+	}
 
-  if accessIdentifier.SecretKey != "kWcrlUX5JEDGM/LtmEENI/aVmYvHNif5zB+d9+ct" {
-    t.Fatalf("incorrect JSON parsing element mismatch", accessIdentifier)  
-  }
+	if accessIdentifier.SecretKey != "kWcrlUX5JEDGM/LtmEENI/aVmYvHNif5zB+d9+ct" {
+		t.Fatalf("incorrect JSON parsing element mismatch", accessIdentifier)
+	}
 
 }
 
 func TestReadLocalFile(t *testing.T) {
-  output := readLocalFile(LOCAL_FILE)
-  if len(output) < 0 {
-    t.Fatalf("Unable to read contents of file")
-  }
+	output, err := readLocalFile(LOCAL_FILE)
+
+	if err != nil {
+		t.Fatal("Error:", err)
+	}
+
+	if len(output) < 0 {
+		t.Fatalf("Unable to read contents of file")
+	}
+}
+
+func TestFindZone(t *testing.T) {
+
 }
